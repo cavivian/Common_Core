e38b4cfccbbc0f051af7135d098b457ba0ee70b1
Camilla Viviani Della Robbia <cavivian@c1r5p12.42firenze.it>
2025-12-23 09:42:37 +0100

    aggiornato

--- printf/ft_external_func.c
+++ printf/ft_external_func.c
@@ -1,18 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*   ft_external_func.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cavivian <cavivian@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/12/23 08:45:37 by cavivian          #+#    #+#             */
-/*   Updated: 2025/12/23 09:20:10 by cavivian         ###   ########.fr       */
+/*   Created: 2025/12/23 09:33:39 by cavivian          #+#    #+#             */
+/*   Updated: 2025/12/23 09:38:48 by cavivian         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int ft_printf(const char *, ...)
+void	ft_putchar(char c)
 {
-	
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		write(1, &str[1], 1);
+		i++;	
+	}
 }
