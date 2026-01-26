123571e45505cc8b7cad00f52c32bad18829c33b
Camilla Viviani Della Robbia <cavivian@c1r5p12.42firenze.it>
2026-01-26 08:49:05 +0100

    23gen

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,12 +6,16 @@
 /*   By: cavivian <cavivian@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/16 11:28:10 by cavivian          #+#    #+#             */
-/*   Updated: 2026/01/22 10:27:46 by cavivian         ###   ########.fr       */
+/*   Updated: 2026/01/23 16:27:54 by cavivian         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE
+#endif
 
 # include <unistd.h>
 # include <stdlib.h>
