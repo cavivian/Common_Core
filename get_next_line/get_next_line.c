0ce44e99b08e2732c1bd03775083f1052d39d17b
Camilla Viviani Della Robbia <cavivian@c1r5p12.42firenze.it>
2026-02-24 11:03:43 +0100

    24feb

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: cavivian <cavivian@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/21 14:02:07 by cavivian          #+#    #+#             */
-/*   Updated: 2026/02/23 16:44:25 by cavivian         ###   ########.fr       */
+/*   Updated: 2026/02/24 10:42:11 by cavivian         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,9 +34,9 @@
 		//i++; // incremento del buffer
 	//}
 	//return (NULL);
-//} */// in sintesi non: aggiorna *str, riduce buffer, ritorn aqualcosa di coerente, fa free di nulla. e' incompleta 
+//} */// in sintesi non: aggiorna *str, riduce buffer, ritorna qualcosa di coerente, fa free di nulla. e' incompleta 
 
-char	*dividestr1(char *buffer) //char *str)
+char	*dividestr1(char *buffer) //char *str) non va bene
 {
 	int		i;
 	int		a;
@@ -62,60 +62,60 @@ char	*dividestr1(char *buffer) //char *str)
 	return (NULL);
 }
 
-char	*dividestr(char	*str)
+char	*endivide(char *str) // duplica la stringa da dopo la \n fino a \0
 {
 	int		len;
 	char	*tmp;
-	char	*vol;
 
 	len = 0;
-	while (str[len] != '\0' || str[len] != '\n')
-		len++;
-	if (ft_strchr(str, '\n'))
-		tmp == ft_strdup(ft_strchr(str, '\n') + 1);
-	else if (!ft_strchr(str, '\n'))
-		tmp == ft_strdup(str);
-	free (str);
-	return (tmp);
+	while (str[len] != '\0' || str[len] != '\n') // scorri la stringa fino alla fine o fino alla \n
+		len++; // qui len contiene la lunghezza
+	if (ft_strchr(str, '\n')) // qiu si dice se lo strchr trova dentro str la \n
+		tmp = ft_strdup(ft_strchr(str, '\n') + 1); // qui si assegna a tmp la duplicazione della stringa, dopo \n fino a \0
+	else if (!ft_strchr(str, '\n')) // qui si fa il controllo se non c'e' la \n
+		tmp = ft_strdup(str); // si assegna comunque a tmp la duplicazione di str
+	free (str); // si libera str -> senno' continuera' a sovrascriversi
+	return (tmp); // si ritorna la stringa duplicata
 }
 
-char 	*startdivide(char 	*str)
+char 	*startdivide(char *str) //duplica la stringa dall'inizio fino alla \n
 {
 	int		len;
 	char	*tmp;
 	
+	len = 0;
 	while (str[len] != '\0' || str[len] != '\n')
 		len++;
-	tmp = malloc(len + 2);
-		if (ft_strchr(str, '\n'))
-		tmp == ft_strdup(ft_strchr(str, '\n') + 1);
-	else if (!ft_strchr(str, '\n'))
-		tmp == ft_strdup(str);
-	free (str);
-	return (tmp);
+	tmp = malloc(len + 2); // quel +2 serve per aggiungere \n e \0
+	if (ft_strchr(str, '\n')) // se strchr trova \n
+		tmp = ft_strdup(ft_strchr(str, '\n') + 1); // tmp contiene la duplicazione della stringa fino alla \n compresa, infatti +1
+	else if (!ft_strchr(str, '\n')) // controllo se non c'e' la \n
+		tmp = ft_strdup(str); // tmp contiene la duplicazione della stringa
+	free (str); // libero str
+	return (tmp); // ritorno tmp che contiene la stringa duplicata
 }
 
 char	*get_next_line(int fd)
 {
 	static char	*str; // persistente tra chiamate, accumulatore della riga corrente
 	char		*m;
-	int			byte;
+	int			byte; // indice
 
-	m = malloc(BUFFER_SIZE + 1); // allochi buffer di lettura
-	if (!m)
-		return (NULL);
+	m = malloc(BUFFER_SIZE + 1); // allochi buffer di lettura +1 che e' lo \0
+	if (!m) // controllo che se m non esiste
+		return (NULL); // ritorno NULL
 	while (!str || (str && ft_strlen(str) > 0 && str[ft_strlen(str) - 1] != '\n')) // continua a leggere finquando str e' NULL, oppure se str non termina con \n.
 	{
 		byte = read(fd, m, BUFFER_SIZE); // leggi da file descriptor
-		if (byte == -1)
+		if (byte == -1) // -1 perche' c'e' il rischio che la stringa sia vuota e che gli sia sottratto il -1 per la \n 
 		{
-			free(m);
-			free(str);
-			return (NULL);
+			free(m); // si libera m
+			free(str); // si libera str
+			return (NULL); // si ritorna NULL
 		}
 		m[byte] = '\0'; // null-termini il buffer appena letto
 		if(!str)
-			str = ft_strdup(m);
+			str = ft_strdup(m); // m perche' contiene la stringa letta
 		else
 			str = ft_strjoin(str, m);
 		free (m);
