0567e6e75e7a1a0abfb0ed0117d902df71cd1c66
Camilla Viviani Della Robbia <cavivian@c1r5p12.42firenze.it>
2026-03-13 16:50:35 +0100

    13mar

--- push_swap/push_swap.c
+++ push_swap/push_swap.c
@@ -6,15 +6,33 @@
 /*   By: cavivian <cavivian@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/03 10:13:10 by cavivian          #+#    #+#             */
-/*   Updated: 2026/03/12 15:59:02 by cavivian         ###   ########.fr       */
+/*   Updated: 2026/03/13 14:12:13 by cavivian         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	easyswap(int *argc, int stack[])
+int	sort_check(int argc, struct Stacks *ab)
 {
-	
+	t_node	*tmp;
+
+	tmp = ab->stack_a;
+	if (ab->stack_a == NULL || ab->stack_a->next == NULL) // controllo che esiste lo stack e che ci sia piu' di un nodo
+		return (0);
+	while (tmp->next != NULL && tmp->value < tmp->next->value)
+	{
+		tmp = tmp->next;
+		if (tmp->value < tmp->next->value)
+		{
+			
+		}
+	}
+	return (1);
+}
+
+void	easyswap(int argc, struct Stacks *a_b)
+{
+	sort_check(argc, a_b);
 }
 
 int push_swap(struct Stacks, int *argc, char *argv)
@@ -32,6 +50,7 @@ int push_swap(struct Stacks, int *argc, char *argv)
 }
 int main( int *argc[], char *argv[])
 {
+	struct Stacks ;
 	if (argc <= 0 || !argv)
 		write(1, "Error", sizeof(char));
 	if (argc <= 5)
