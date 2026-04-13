105c4ed662cc4f52f1a83ba5507900cad8a7a219
cavivian <millavdr@gmail.com>
2026-04-13 08:25:00 +0200

    13apr

--- Python/Module 01/ex03/ft_plant_factory.py
+++ Python/Module 01/ex03/ft_plant_factory.py
@@ -1,3 +1,19 @@
 class Plant:
-	def __init__(self):
-		pass
+	def __init__(self, name: str, height: float, age: int) -> None:
+		self.name = name
+		self.height = height
+		self.days = age
+
+	def show(self) -> str:
+		return f"Created: {self.name}: {self.height:.1f}cm, {self.days} days old"
+	
+	def	grow(self) -> int:
+		self.height = float
+
+def main() ->None:
+	print("=== Plant Factory Output ===")
+	nomepianta1 = Plant('Rose', 25, 30)
+	print(nomepianta1.show())
+
+if __name__ == "__main__":
+	main()
