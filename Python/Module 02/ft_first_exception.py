d9c9df4891230e2d92d09779e6f8a19e3d2c17c0
cavivian <millavdr@gmail.com>
2026-04-20 08:22:20 +0200

    20apr

--- Python/Module 02/ft_first_exception.py
+++ Python/Module 02/ft_first_exception.py
@@ -1,3 +1,19 @@
 def input_temperature(tmp_str):
 	print('=== Garden Temperature ===')
-	tmp_str = str(input('Input data is: '))
+	
+	print(f"Input data is '{tmp_str}'")
+	print(f"Temperature is now {tmp_str}°C")
+
+def test_temperature(tmp_str):
+	input_temperature(25)
+	input_temperature('abc')
+	if tmp_str == str:
+		print("Caught input_temperature error: invalid literal for int() with base 10: 'abc'")
+
+
+def main():
+	input_temperature(25)
+	test_temperature(25)
+
+if __name__ == "__main__":
+	main()
