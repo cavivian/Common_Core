3fe0a46c4ddf9dc0380baa83e238c3dc757247f8
cavivian <millavdr@gmail.com>
2026-05-03 11:35:32 +0200

    3mag

--- Python/Module 03/ex02/ft_coordinate_system.py
+++ Python/Module 03/ex02/ft_coordinate_system.py
@@ -1,18 +1,36 @@
 import math
 
 def get_player_pos():
-    print('\nGet a first set of coordinates')
-    try:
-        input(int("Enter new coordinates as floats in format 'x,y,z': "))
-        print(f"Got a first tuple: ({input(tuple())})")
-    except:
-        print('Invalid syntax')
-
-
+	while True:
+		line = input("Enter new coordinates as floats in format 'x,y,z': ")
+		try:
+			part = line.split(',')
+			if len(part) != 3:
+				print('Invalid syntax')
+				continue
+			coordinata = (float(part[0].strip()), float(part[1].strip()), float(part[2].strip()))
+			return coordinata
+		except ValueError as e:
+			for p in part:
+				try:
+					float(p.strip())
+				except ValueError:
+					print(f"Error on parameter '{p.strip()}': {e}")
+					break
 
 def main():
-    print('=== Game Coordinate System ===')
-    get_player_pos()
+	print('=== Game Coordinate System ===')
+	print('\nGet a first set of coordinates')
+	part1 = get_player_pos()
+	print(f'Got a firts tuple: ({part1})')
+	print(f'It includes: X={part1[0]}, Y={part1[1]}, Z={part1[2]}')
+	distance1 = math.sqrt((part1[0])**2 + (part1[1])**2 + (part1[2])**2)
+	print(f'Distance to center: {round(distance1, 4)}')
+	print('\nGot a second set of coordinates')
+	part2 = get_player_pos()
+	distance2 = math.sqrt((part2[0] - part1[0])**2 + (part2[1] - part1[1])**2 + (part2[2] - part1[2])**2)
+	print(f'Distance between the 2 sets of coordinates: {round(distance2, 4)}')
+
 
 if __name__ == "__main__":
-    main()
+	main()
