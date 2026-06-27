7a756e7a5fdf0b2283ab44d65c6fc7878ebbae25
cavivian <millavdr@gmail.com>
2026-06-27 10:22:05 +0200

    27giu

---
+++ a_maze_ing/maze_gen/path.py
@@ -0,0 +1,69 @@
+NORTH = 1
+EAST  = 2
+SOUTH = 4
+WEST  = 8
+
+DX = {NORTH: 0, SOUTH: 0, EAST: 1, WEST: -1}
+DY = {NORTH: -1, SOUTH: 1, EAST: 0, WEST: 0}
+
+DIR_LETTERA = {NORTH: 'N', EAST: 'E', SOUTH: 'S', WEST: 'W'}
+
+
+def il_path(grid, start, end):
+    width = len(grid[0])
+    height = len(grid)
+
+    sx, sy = start #per coord di patenza
+    ex, ey = end
+
+    # giri per vedere le celle visitate
+    visited = []
+    for y in range(height):
+        row = []
+        for x in range(width):
+            row.append(False)
+        visited.append(row)
+
+    parent = {} # diz che ricorda dove siamo arrivati x ogni cella
+
+    stack = [(sx, sy)] # pila dfs da cella di inizio
+    visited[sy][sx] = True
+
+    while stack: # prende ultim cell inser nella pila 
+        x, y = stack.pop()
+
+        if (x, y) == (ex, ey): # se è a fine ritracciamo il percorso
+            return ricostruz_percor(parent, start, end)
+
+        for direction in [NORTH, EAST, SOUTH, WEST]: # prova tutte e 4 dir
+
+            chiuso = grid[y][x] & direction
+            if chiuso:
+                continue
+
+            nx = x + DX[direction] # coord cella vicina
+            ny = y + DY[direction]
+
+            dentro_x = 0 <= nx < width
+            dentro_y = 0 <= ny < height
+
+            if dentro_x and dentro_y:
+                if not visited[ny][nx]:
+                    visited[ny][nx] = True #sega come visitata
+                    parent[(nx, ny)] = (x, y, direction) # ricorda arrivo
+                    stack.append((nx, ny)) # aggiunge a pila
+
+    return None # se pila vuota e niente fine non esiste percorso
+
+
+def ricostruz_percor(parent, start, end):
+    strada = []
+    current = end
+
+    while current != start: # risale al percc seguedo paret
+        x, y, direction = parent[current]
+        il_path.append(DIR_LETTERA[direction]) # aggiuge lettera direz
+        current = (x, y)
+
+    strada.reverse() #percorso al contrario quindi inverte
+    return ''.join(strada) #unisce tutte lettere a stringa tio sequenza di lette boh NNEEWWSS etc
--- prove.c
+++ prove.c
@@ -26,33 +26,132 @@
 
 
 //	ES-2- 
-char print_numb(int c)
-{
-	if (c >= 10)
-		print_numb(c / 10);
-	c = c % 10 + 48;
-	write(1, &c, 1);
-}
+// char print_numb(int c)
+// {
+// 	if (c >= 10)
+// 		print_numb(c / 10);
+// 	c = c % 10 + 48;
+// 	write(1, &c, 1);
+// }
 
-void fizzbuzz()
+// void fizzbuzz()
+// {
+// 	int n = 1;
+// 	while (n <= 10 * 10)
+// 	{
+// 		if (n % 3 == 0 && n % 5 != 0)
+// 			write(1, "fizz", 4);
+// 		else if (n % 5 == 0 && n % 3 != 0)
+// 			write(1, "buzz", 4);
+// 		else if (n % 5 == 0 && n % 3 == 0)
+// 			write(1, "fizzbuzz", 1);
+// 		else
+// 			print_numb(n);
+// 		n++;
+// 		write(1, "\n", 1);
+// 	}
+// }
+
+// int main()
+// {
+// 	fizzbuzz();
+// }
+
+
+//	ES-3-
+// void ft_putstr(char *str)
+// {
+// 	int i = 0;
+// 	while (str[i] != '\0')
+// 	{
+// 		write(1, &str[i], 1);
+// 		i++;
+// 	}
+// }
+
+// int main()
+// {
+// 	char *str = "ciao mamma";
+// 	ft_putstr(str);
+// 	return 0;
+// }
+
+
+//	ES-4-
+// char *ft_strcpy(char *s1, char *s2)
+// {
+// 	int i = 0;
+// 	while(s2[i] != '\0')
+// 	{
+// 		s1[i] = s2[i];
+// 		i++;
+// 	}
+// 	return(s2);
+// }
+
+// int main()
+// {
+// 	char *sorgente = "ciao mamma";
+// 	char dest[100];
+// 	printf("%s\n", ft_strcpy(sorgente, dest));
+// 	return(0);
+// }
+
+
+//	ES-5-
+// int ft_strlen(char *str)
+// {
+// 	int i = 0;
+// 	while (str[i] != '\0')
+// 	{
+// 		i++;
+// 	}
+// 	return (i);
+// }
+
+// int main()
+// {
+// 	char *str = "ciao mamma";
+// 	printf("%d", ft_strlen(str));
+// 	return (0);
+// }
+
+
+//	ES-6-
+// void	ft_swap(int *a, int *b)
+// {
+// 	int tmp;
+// 	tmp = *a;
+// 	*a = *b;
+// 	*b = tmp;
+// }
+
+// int main()
+// {
+// 	int a = 5;
+// 	int b = 6;
+// 	ft_swap(&a, &b);
+// 	printf("%d\n", a);
+// 	printf("%d\n", b);
+// 	return 0;
+// }
+
+
+//	ES-7- (last_word)
+int main(int argc, char *argv[])
 {
-	int n = 1;
-	while (n <= 10 * 10)
+	if (argc != 2)
 	{
-		if (n % 3 == 0 && n % 5 != 0)
-			write(1, "fizz", 4);
-		else if (n % 5 == 0 && n % 3 != 0)
-			write(1, "buzz", 4);
-		else if (n % 5 == 0 && n % 3 == 0)
-			write(1, "fizzbuzz", 1);
-		else
-			print_numb(n);
-		n++;
-		write(1, "\n", 1);
+		write (1, "\n", 1);
+		return 0;
 	}
-}
-
-int main()
-{
-	fizzbuzz();
+	int i = 0;
+	while (argv[1][i] != '\0')
+	{
+		i++;
+	}
+	i--;
+	if (argv[1][i] == 32 || (argv[1][i] >= 9 && argv[1][i] <= 13))
+		i--;
+	while (argv >= 1)
 }
