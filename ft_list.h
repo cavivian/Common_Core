82b1a15e9d417f64b770db8ff00ecf42c33776b0
Camilla Viviani Della Robbia <cavivian@c1r5p12.42firenze.it>
2026-07-06 18:50:51 +0200

    6lug

--- ft_list.h
+++ ft_list.h
@@ -1,9 +1,14 @@
 #ifndef FT_LIST_H
 # define  FT_LIST_H
-typedef struct s_list
-{
-    struct s_list *next;
-    void *data;
-}   t_list;
+// typedef struct s_list
+// {
+//     struct s_list *next;
+//     void *data;
+// }   t_list;
 
+typedef struct  s_point
+  {
+    int           x;
+    int           y;
+  }               t_point;
 #endif
--- prove.c
+++ prove.c
@@ -1335,3 +1335,66 @@
 // }
 
 
+//	- - - - LIVELLO 4 - - - -
+//	ES -1- (flood fill)
+void fill_helper(char **tab, t_point size, int y, int x, char base)
+{
+	if (y < 0 || y >= size.y)
+		return ;
+	else if (x < 0 || y >= size.x)
+		return ;
+	else if (tab[x][y] != base)
+		return ;
+	else
+	{
+		tab[y][x] = 'F';
+		fill_helper(tab, size, y, x + 1, base);
+        fill_helper(tab, size, y, x - 1, base);
+        fill_helper(tab, size, y + 1, x, base);
+        fill_helper(tab, size, y - 1, x, base);
+	}
+}
+void flood_fill(char **tab, t_point size, t_point begin)
+{
+	char base = tab[begin.y, begin.x];
+	fill_helper(tab, size, begin.y, begin.x, base);
+}
+
+char** make_area(char** zone, t_point size)
+{
+	char** new;
+
+	new = malloc(sizeof(char*) * size.y);
+	for (int i = 0; i < size.y; ++i)
+	{
+		new[i] = malloc(size.x + 1);
+		for (int j = 0; j < size.x; ++j)
+			new[i][j] = zone[i][j];
+		new[i][size.x] = '\0';
+	}
+
+	return new;
+}
+
+int main(void)
+{
+	t_point size = {8, 5};
+	char *zone[] = {
+		"11111111",
+		"10001001",
+		"10010001",
+		"10110001",
+		"11100001",
+	};
+
+	char**  area = make_area(zone, size);
+	for (int i = 0; i < size.y; ++i)
+		printf("%s\n", area[i]);
+	printf("\n");
+
+	t_point begin = {7, 4};
+	flood_fill(area, size, begin);
+	for (int i = 0; i < size.y; ++i)
+		printf("%s\n", area[i]);
+	return (0);
+}
